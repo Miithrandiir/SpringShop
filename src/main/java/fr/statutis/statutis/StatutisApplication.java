3896e3c6c9ea00c4fd574fbc3ac93d62f33316e6
Ludwig SILVAIN <ludwig@silvain.eu>
2023-02-02 11:03:10 +0100

    feat : init

---
+++ src/main/java/fr/statutis/statutis/StatutisApplication.java
@@ -0,0 +1,13 @@
+package fr.statutis.statutis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatutisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatutisApplication.class, args);
+    }
+
+}
