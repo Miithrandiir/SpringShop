4e55021fc3390a682308f1dc32316fabde3ec2bb
Ludwig SILVAIN <ludwig@silvain.eu>
2023-02-22 15:14:08 +0100

    feat(products) : hateoas pour les produits

--- src/main/java/fr/ulco/springshop/controllers/ProductController.java
+++ src/main/java/fr/ulco/springshop/controllers/ProductController.java
@@ -6,11 +6,14 @@ import fr.ulco.springshop.model.form.ProductForm;
 import fr.ulco.springshop.service.core.ProductServiceInterface;
 import fr.ulco.springshop.service.core.StorageServiceInterface;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,9 +29,23 @@ public class ProductController {
         return ResponseEntity.ok(productService
                 .findAll()
                 .stream()
-                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), x.getThumbnail()))
+                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), getRouteProductThumbnail(x)))
                 .collect(Collectors.toList())
         );
+    }
+
+    @GetMapping(Routes.GET_PRODUCTS_THUMBNAIL)
+    public ResponseEntity<Resource> getProductsThumbnail(@PathVariable int id) {
+        Optional<ProductBO> productBO = productService.findById(id);
+        if (productBO.isPresent() && productBO.get().getThumbnail() != null) {
+
+            Resource resource = storageService.loadAsResource(productBO.get().getThumbnail());
+            return
+                    ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                                    "attachment; filename=\"" + productBO.get().getThumbnail() + "\"")
+                            .body(resource);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping(Routes.GET_PRODUCTS_BY_CATEGORY)
@@ -36,7 +53,7 @@ public class ProductController {
         return ResponseEntity.ok(productService
                 .findByCategory(slug)
                 .stream()
-                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), x.getThumbnail()))
+                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), getRouteProductThumbnail(x)))
                 .collect(Collectors.toList())
         );
     }
@@ -46,7 +63,7 @@ public class ProductController {
         return ResponseEntity.ok(productService
                 .findByHighlighted()
                 .stream()
-                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), x.getThumbnail()))
+                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), getRouteProductThumbnail(x)))
                 .collect(Collectors.toList())
         );
     }
@@ -55,7 +72,7 @@ public class ProductController {
     public ResponseEntity<ProductDTO> getProductById(@PathVariable int id) {
         return ResponseEntity.ok(productService
                 .findById(id)
-                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), x.getThumbnail()))
+                .map(x -> new ProductDTO(x.getId(), x.getName(), x.getPrice(), x.getQuantity(), x.getDescription(), getRouteProductThumbnail(x)))
                 .orElse(null));
     }
 
@@ -68,7 +85,14 @@ public class ProductController {
         ProductBO createdBo = productService
                 .save(new ProductBO(productForm.getName(), productForm.getPrice(), productForm.getQuantity(), productForm.getDescription(), thumbnail));
 
-        return ResponseEntity.ok(new ProductDTO(createdBo.getId(), createdBo.getName(), createdBo.getPrice(), createdBo.getQuantity(), createdBo.getDescription(), createdBo.getThumbnail()));
+        return ResponseEntity.ok(new ProductDTO(createdBo.getId(), createdBo.getName(), createdBo.getPrice(), createdBo.getQuantity(), createdBo.getDescription(), getRouteProductThumbnail(createdBo)));
+    }
+
+
+    private String getRouteProductThumbnail(ProductBO productBO) {
+        if (productBO.getThumbnail() == null)
+            return null;
+        return Routes.GET_PRODUCTS_THUMBNAIL.replace("{id}", String.valueOf(productBO.getId()));
     }
 
 }
--- src/main/java/fr/ulco/springshop/controllers/Routes.java
+++ src/main/java/fr/ulco/springshop/controllers/Routes.java
@@ -13,6 +13,7 @@ public class Routes {
      */
     public static final String GET_PRODUCTS = "/products";
     public static final String GET_PRODUCT_BY_ID = "/products/{id}";
+    public static final String GET_PRODUCTS_THUMBNAIL = GET_PRODUCT_BY_ID + "/thumbnail";
     public static final String GET_PRODUCTS_BY_CATEGORY = "/products/category/{slug}";
     public static final String GET_HIGHLIGHTED_PRODUCTS = "/products/highlighted";
     public static final String POST_PRODUCTS = GET_PRODUCTS;
