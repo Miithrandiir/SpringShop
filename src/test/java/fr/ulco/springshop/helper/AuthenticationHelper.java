78b7cedc55998c9d91a5d5ebae86487bb9916767
Ludwig SILVAIN <ludwig@silvain.eu>
2023-03-09 10:51:01 +0100

    feat(test) : AuthenticationHelperPostProcessor

--- src/test/java/fr/ulco/springshop/helper/AuthenticationHelper.java
+++ src/test/java/fr/ulco/springshop/helper/AuthenticationHelper.java
@@ -5,7 +5,9 @@ import fr.ulco.springshop.security.jwt.JwtTokenUtil;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +22,31 @@ public class AuthenticationHelper {
 
     public String generateToken(String username) {
         return jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername(username));
+    }
+
+    public RequestPostProcessor jwt(String username){
+        return new AuthenticationHelperPostProcessor(generateToken(username));
+    }
+
+    public static RequestPostProcessor customJwt(String token){
+        return new AuthenticationHelperPostProcessor(token);
+    }
+
+    public static class AuthenticationHelperPostProcessor implements RequestPostProcessor {
+
+        private final String token;
+
+        private AuthenticationHelperPostProcessor(String token) {
+            this.token = token;
+        }
+
+        @Override
+        public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+
+            request.addHeader("Authorization", "Bearer " + this.token);
+
+            return request;
+        }
     }
 
 }
